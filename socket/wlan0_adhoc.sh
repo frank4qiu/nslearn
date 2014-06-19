77ecac31345483e24f679e74846612acc23c8ff5
frank4qiu <frank4qiu@gmail.com>
2014-06-19 15:51:07 +0800

    加上脚本

---
+++ socket/wlan0_adhoc.sh
@@ -0,0 +1,5 @@
+ifconfig wlan0 down
+iwconfig wlan0 mode ad-hoc
+iwconfig wlan0 essid "adhoc_A"
+ifconfig wlan0 up
+ifconfig wlan0 192.168.2.1
---
+++ socket/wlan1_adhoc.sh
@@ -0,0 +1,5 @@
+ifconfig wlan1 down
+iwconfig wlan1 mode ad-hoc
+iwconfig wlan1 essid "adhoc_B"
+ifconfig wlan1 up
+ifconfig wlan1 192.168.2.3
